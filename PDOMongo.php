bf28bd9931b47c203407e0a18cb9c3ca3a49e6e0
Pablo Henrique <pablohenrique@mint>
2013-06-24 11:14:47 -0300

    new update

--- PDOMongo.php
+++ PDOMongo.php
@@ -17,7 +17,7 @@ class PDOMongo{
 				$serverString .= $host . "/" . $dbauth;
 			else
 				$serverString .= $username . ":" . $password . "@" . $host . "/" . $dbauth;
-			
+
 			$this->connection = new MongoClient($serverString);
 			self::setDatabase($database);
 		}
@@ -27,18 +27,18 @@ class PDOMongo{
 	}
 
 	/*
-	 * [PRIVATE] Create an Object (array) using a mongoCursor.
-	 */
+	* [PRIVATE] Create an Object (array) using a mongoCursor.
+	*/
 	private function createObjects($mongoCursor){
 		$resultSet = array();
-        while($mongoCursor->hasNext())
-            $resultSet[] = $mongoCursor->getNext();
-        return $resultSet;
+		while($mongoCursor->hasNext())
+			$resultSet[] = $mongoCursor->getNext();
+		return $resultSet;
 	}
-	
+
 	/*
-	 * [PRIVATE] Create an array for search purposes;
-	 */
+	* [PRIVATE] Create an array for search purposes;
+	*/
 	private function createArray($index, $value){
 		if($index == 'id')
 			return array('_id' => new MongoID($value));
@@ -46,9 +46,15 @@ class PDOMongo{
 			return array($index => $value);
 	}
 
-	public function setDatabase($input){ if(!is_null($input)) $this->database = $this->connection->selectDB($input); }
+	public function setDatabase($input){ 
+		if(!is_null($input)) 
+			$this->database = $this->connection->selectDB($input); 
+	}
 
-	public function setCollection($input){ if(!is_null($input)) $this->collection = $this->database->selectCollection($input); }
+	public function setCollection($input){ 
+		if(!is_null($input)) 
+			$this->collection = $this->database->selectCollection($input); 
+	}
 
 	public function getConnection(){ return $this->connection; }
 
@@ -61,21 +67,21 @@ class PDOMongo{
 	public function update($f1, $f2){ $this->collection->update($f1, $f2); }
 
 	public function ensureIndex($args){ return $this->collection->ensureIndex($args); }
- 
-    public function get($index, $value){
-        $mongoCursor = $this->collection->find(self::createArray($index, $value));
-        return self::createObjects($mongoCursor);
-    }
- 
-    public function getAll(){
-        $mongoCursor = $this->collection->find();
-        return self::createObjects($mongoCursor);
-    }
- 
-    public function delete($field, $value, $justOne = FALSE){
-        $c = $this->collection->remove(self::createArray($field, $value), self::createArray('justOne', $justOne));
-        return $c;
-    }
+
+	public function get($index, $value){
+		$mongoCursor = $this->collection->find(self::createArray($index, $value));
+		return self::createObjects($mongoCursor);
+	}
+
+	public function getAll(){
+		$mongoCursor = $this->collection->find();
+		return self::createObjects($mongoCursor);
+	}
+
+	public function delete($field, $value, $justOne = FALSE){
+		$c = $this->collection->remove(self::createArray($field, $value), self::createArray('justOne', $justOne));
+		return $c;
+	}
 }
  #
  # Exemplo de uso do PDOMongo passando o host e o banco de dados
